e15c2a359fc0dd1a13926eba43175cf541f7ab08
Lukas Müller <WookieFPV@users.noreply.github.com>
2025-12-12 15:59:10 +0100

    fix: add compatibility to more expo versions (by dropping @expo/config-plugins and loosen the ExpoConfig type)

--- src/plugin/types.ts
+++ src/plugin/types.ts
@@ -1,4 +1,7 @@
-import type { ConfigPlugin } from "@expo/config-plugins";
+// Ignore ExpoConfig to support more versions of expo-config
+type ExpoConfig = any;
+
+export type ConfigPlugin<Props = void> = (config: ExpoConfig, props: Props) => ExpoConfig;
 
 // biome-ignore lint/suspicious/noConfusingVoidType: ...
 export type ConfigPluginOptions<T> = T extends ConfigPlugin<infer TOptions> ? Exclude<TOptions, void | undefined> : never;
